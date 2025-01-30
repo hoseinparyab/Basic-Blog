419932e8e17fa37e277b220dac636afb0b48893c
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-01-30 16:49:14 +0330

    Edit posts

--- panel/post/index.php
+++ panel/post/index.php
@@ -2,8 +2,9 @@
 require_once '../../functions/helpers.php';
 require_once '../../functions/pdo_connection.php';
 
-?>
 
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,19 +17,19 @@ require_once '../../functions/pdo_connection.php';
 <body>
 <section id="app">
 
-    <?php require_once '../layouts/top-nav.php' ?>
+    <?php require_once '../layouts/top-nav.php'; ?>
 
     <section class="container-fluid">
         <section class="row">
             <section class="col-md-2 p-0">
-                <?php require_once '../layouts/sidebar.php' ?>
+                <?php require_once '../layouts/sidebar.php'; ?>
 
             </section>
             <section class="col-md-10 pt-3">
 
                 <section class="mb-2 d-flex justify-content-between align-items-center">
                     <h2 class="h4">Articles</h2>
-                    <a href="create.php" class="btn btn-sm btn-success">Create</a>
+                    <a href="<?= url('panel/post/create.php') ?>" class="btn btn-sm btn-success">Create</a>
                 </section>
 
                 <section class="table-responsive">
@@ -38,7 +39,7 @@ require_once '../../functions/pdo_connection.php';
                             <th>#</th>
                             <th>image</th>
                             <th>title</th>
-                            <th>cat_id</th>
+                            <th>category name</th>
                             <th>body</th>
                             <th>status</th>
                             <th>setting</th>
@@ -47,38 +48,37 @@ require_once '../../functions/pdo_connection.php';
                         <tbody>
                         <?php
                         global $pdo;
-                        $query = "SELECT blog_php.posts .*, blog_php.categories.name AS category_name FROM  
-                        blog_php.posts LEFT JOIN blog_php.categories ON blog_php.posts.cat_id = blog_php.categories.id";
+                        $query = "SELECT blog_php.posts.*, blog_php.categories.name AS category_name FROM blog_php.posts LEFT JOIN blog_php.categories ON blog_php.posts.cat_id = blog_php.categories.id";
                         $statement = $pdo->prepare($query);
                         $statement->execute();
                         $posts = $statement->fetchAll();
                         foreach ($posts as $post) {
-                        ?>
-                        <tr>
-                            <td><?= $post->id ?></td>
-                            <td>
-<!--                                <img style="width: 90px;" src="">-->
-                            </td>
-                            <td><?= $post->title ?></td>
-                            <td><?= $post->category_name ?></td>
-                            <td><?= substr($post->body ,0,30). '...'  ?></td>
-                            <td>
-                                <?php if ($post->status==10) {?>
-                                <span class="text-success">enable</span>
-                               <?php }else{?>
-                                <span class="text-danger">disable</span></td>
-                                <?php }?>
-                            <td>
-                                <a href="" class="btn btn-warning btn-sm">Change status</a>
-                                <a href="" class="btn btn-info btn-sm">Edit</a>
-                                <a href="" class="btn
-                                btn-danger
-                                btn-sm">Delete</a>
-                            </td>
-                        </tr>
-                        <?php
-                        }
-                        ?>
+                            ?>
+                            <tr>
+                                <td><?= $post->id ?></td>
+                                <td>
+                                    <img style="width: 90px;" src="<?= asset($post->image) ?>">
+                                </td>
+                                <td><?= $post->title ?></td>
+                                <td><?= $post->category_name ?></td>
+                                <td><?= substr($post->body, 0 ,30) . ' ...'  ?></td>
+                                <td>
+                                    <?php if($post->status == 10) { ?>
+                                        <span class="text-success">enable</span>
+                                    <?php } else { ?>
+                                        <span class="text-danger">disable</span>
+                                    <?php } ?>
+                                </td>
+                                <td>
+                                    <a href="" class="btn btn-warning btn-sm">Change status</a>
+                                    <a href="<?= url('panel/post/edit.php?post_id=' . $post->id) ?>" class="btn btn-info btn-sm">Edit</a>
+                                    <a href="" class="btn btn-danger btn-sm">Delete</a>
+                                </td>
+                            </tr>
+
+                            <?php
+                        } ?>
+
                         </tbody>
                     </table>
                 </section>
